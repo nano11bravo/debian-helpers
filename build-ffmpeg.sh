fd31ee16618379edae1ed176983890e87a7f37a3
𝖓𝖆𝖓𝖔11𝖇𝖗𝖆𝖛𝖔 <samuel.walton@seven-labs.com>
2025-01-22 14:11:31 -0500

    Create build-ffmpeg.sh
    A simple bash script to help with compiling ffmpeg and associated modules.

---
+++ build-ffmpeg.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# Bash script to help build
+# ffmpeg with common modules.
+
+# Nano11Bravo <nano@seven-labs.com>
+
+# Require root or exit (we're compiling after all)
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# can be commented out if you already did this step
+yum install autoconf automake bzip2 bzip2-devel cmake freetype-devel gcc gcc-c++ git libtool make pkgconfig zlib-devel
+
+# create directory for ffmpeg sources
+mkdir ~/ffmpeg_sources
+
+# NASM - An assembler used by some libraries. Highly recommended or your resulting build may be very slow.
+cd ~/ffmpeg_sources
+curl -O -L https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2
+tar xjvf nasm-2.15.05.tar.bz2
+cd nasm-2.15.05
+./autogen.sh
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+
+# YASM - An assembler used by some libraries. Highly recommended or your resulting build may be very slow.
+cd ~/ffmpeg_sources
+curl -O -L https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar xzvf yasm-1.3.0.tar.gz
+cd yasm-1.3.0
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+
+
+# libx264 - H.264 video encoder.
+cd ~/ffmpeg_sources
+git clone --branch stable --depth 1 https://code.videolan.org/videolan/x264.git
+cd x264
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+make
+make install
+
+
+# libx265 - H.265/HEVC video encoder.
+cd ~/ffmpeg_sources
+git clone --branch stable --depth 2 https://bitbucket.org/multicoreware/x265_git
+cd ~/ffmpeg_sources/x265_git/build/linux
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+make
+make install
+
+
+# libfdk_aac - AAC audio encoder.
+cd ~/ffmpeg_sources
+git clone --depth 1 https://github.com/mstorsjo/fdk-aac
+cd fdk-aac
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+# libmp3lame - MP3 audio encoder.
+cd ~/ffmpeg_sources
+curl -O -L https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz
+tar xzvf lame-3.100.tar.gz
+cd lame-3.100
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
+make
+make install
+
+# libopus - Opus audio decoder and encoder.
+cd ~/ffmpeg_sources
+curl -O -L https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
+tar xzvf opus-1.3.1.tar.gz
+cd opus-1.3.1
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+# libvpx - VP8/VP9 video encoder and decoder
+cd ~/ffmpeg_sources
+git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
+cd libvpx
+./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm
+make
+make install
+
+
+# FFMPEG
+cd ~/ffmpeg_sources
+curl -O -L https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar xjvf ffmpeg-snapshot.tar.bz2
+cd ffmpeg
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+  --prefix="$HOME/ffmpeg_build" \
+  --pkg-config-flags="--static" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" \
+  --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --extra-libs=-lpthread \
+  --extra-libs=-lm \
+  --bindir="$HOME/bin" \
+  --enable-gpl \
+  --enable-libfdk_aac \
+  --enable-libfreetype \
+  --enable-libharfbuzz \
+  --enable-libmp3lame \
+  --enable-libopus \
+  --enable-libvpx \
+  --enable-libx264 \
+  --enable-libx265 \
+  --enable-nonfree
+make
+make install
+hash -d ffmpeg
